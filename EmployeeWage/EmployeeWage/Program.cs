0fad70e74ec70bf5f8dc303342dc1f6886535552
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-12-15 17:20:48 +0530

    Employee Wage Executed

---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Is_Full_Time = 1;
+
+            int Emp_Rate_Per_Hour = 10;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee Is Present");
+                empHrs = 8;
+
+            }
+
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+                empHrs = 0;
+            }
+            empWage = empHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("empWage =" + empWage);
+            Console.ReadKey();
+
+        }
+    }
+}
