56b2abca031a884de2b47c8b7d55dcd500f81c6e
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-12-15 17:18:27 +0530

    UC_1

---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Is_Full_Time = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+               
+            }
+
+            else
+
+            {
+                Console.WriteLine("Employee is Absent");
+                Console.ReadKey();
+
+            }
+
+        }
+    }
+}
